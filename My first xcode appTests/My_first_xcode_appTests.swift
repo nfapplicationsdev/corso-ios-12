0b63831f500a98d87eb03e392bee137ad61875ea
nfapplicationsdev <nfapplicationsdev@gmail.com>
2020-04-15 06:55:30 +0200

    Initial Commit

---
+++ My first xcode appTests/My_first_xcode_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  My_first_xcode_appTests.swift
+//  My first xcode appTests
+//
+//  Created by Nicolò Furri on 15/04/2020.
+//  Copyright © 2020 nfapplicationsdev. All rights reserved.
+//
+
+import XCTest
+@testable import My_first_xcode_app
+
+class My_first_xcode_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
